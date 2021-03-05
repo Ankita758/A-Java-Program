0a58412e9172334447a8468d5bd47163f44ed782
Ankita758 <80101628+Ankita758@users.noreply.github.com>
2021-03-05 16:34:08 +0530

    Created Subtract.java
    This file subtracts two numbers 45 and 27 and prints the difference

---
+++ Subtract.java
@@ -0,0 +1,9 @@
+public class Subtract
+{
+      public static void main(String[]args)
+      {
+           int num1=45,num2=27,difference;
+           difference=num1-num2;
+           System.out.println("Difference of given two numbers is:" +difference);
+       }
+}
